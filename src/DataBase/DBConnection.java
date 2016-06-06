1afd6eabf9d8c2182650a14620bdfa7609b682d9
Boris Nizharadze <bnizh14@freeuni.edu.ge>
2016-06-06 18:57:41 +0400

    connectionis da servletis errorebis gasworeba

--- src/DataBase/DBConnection.java
+++ src/DataBase/DBConnection.java
@@ -8,15 +8,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    public DBConnection(){
+    public  Connection getConnection(){
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
         try (
-                Connection con = DriverManager.getConnection("jdbc:mysql://" + MyDBInfo.MYSQL_DATABASE_SERVER + "/" + MyDBInfo.MYSQL_DATABASE_NAME +
-                        "?characterEncoding=UTF8&useSSL=false", MyDBInfo.MYSQL_USERNAME, MyDBInfo.MYSQL_PASSWORD)) {
+                Connection con = DriverManager.getConnection("jdbc:mysql://" + DBInfo.MYSQL_DATABASE_SERVER + "/" + DBInfo.MYSQL_DATABASE_NAME +
+                        "?characterEncoding=UTF8&useSSL=false", DBInfo.MYSQL_USERNAME, DBInfo.MYSQL_PASSWORD)) {
 
             return con;
         } catch (SQLException ex) {

11be0cefaefeb277dbf49418a210fb99b8bb5ef3
Boris <bnizh14@freeuni.edu.ge>
2016-06-07 14:17:45 +0400

    tests of seller

---
+++ src/Objects/SellerTest.java
@@ -0,0 +1,122 @@
+package Objects;
+
+import junit.framework.TestCase;
+
+import javax.ejb.BeforeCompletion;
+
+/**
+ * Created by Boris on 06.06.2016.
+ */
+public class SellerTest extends TestCase {
+
+
+
+    public void testGetGallery() throws Exception {
+
+    }
+
+    public void testSetGallery() throws Exception {
+
+    }
+
+    public void testEquals() throws Exception {
+        Seller s = new Seller("username","password","email","name",0,"112",0, "image");
+        Seller s1 = new Seller("username","password","email","name",0,"112",0, "image");
+        assertTrue(s.equals( s1));
+        Seller s2 = new Seller("username","password","email","name",0,"113",0, "image");
+        assertFalse(s.equals(s2));
+    }
+
+    public void testGetEmail() throws Exception {
+        Seller s = new Seller("username","password","email","name",0,"112",0, "image");
+        assertEquals(s.getEmail(), "email");
+        s = new Seller("username","password","bla1","name",0,"112",0, "image");
+        assertEquals(s.getEmail(), "bla1");
+        assertFalse(s.getEmail().equals("email"));
+    }
+
+    public void testSetEmail() throws Exception {
+        Seller s = new Seller("username","password","email","name",0,"112",0, "image");
+        assertEquals(s.getEmail(), "email");
+        s.setEmail("bla2");
+        assertFalse(s.getEmail().equals("email"));
+        assertEquals(s.getEmail(), "bla2");
+    }
+
+    public void testGetUserName() throws Exception {
+        Seller s = new Seller("username","password","email","name",0,"112",0, "image");
+        assertEquals(s.getUserName(), "username");
+        s = new Seller("bla1","password","email","name",0,"112",0, "image");
+        assertFalse(s.getUserName().equals("username"));
+        assertEquals(s.getUserName(), "bla1");
+    }
+
+    public void testSetUserName() throws Exception {
+        Seller s = new Seller("username","password","email","name",0,"112",0, "image");
+        assertEquals(s.getUserName(), "username");
+        s.setUserName("bla2");
+        assertFalse(s.getUserName().equals("username"));
+        assertEquals(s.getUserName(), "bla2");
+    }
+
+    public void testGetName() throws Exception {
+
+    }
+
+    public void testSetName() throws Exception {
+
+    }
+
+    public void testGetRating() throws Exception {
+
+    }
+
+    public void testSetRating() throws Exception {
+
+    }
+
+    public void testGetImage() throws Exception {
+
+    }
+
+    public void testSetImage() throws Exception {
+
+    }
+
+    public void testGetPassword() throws Exception {
+
+    }
+
+    public void testSetPassword() throws Exception {
+
+    }
+
+    public void testGetMobileNumber() throws Exception {
+
+    }
+
+    public void testSetMobileNumber() throws Exception {
+
+    }
+
+    public void testGetVoters() throws Exception {
+
+    }
+
+    public void testSetVoters() throws Exception {
+
+    }
+
+    public void testInceaseRating() throws Exception {
+
+    }
+
+    public void testGetID() throws Exception {
+
+    }
+
+    public void testSetID() throws Exception {
+
+    }
+
+}

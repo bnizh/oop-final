4c8a9ce60be8674f1512a443f7549fe8d932d0ef
Boris <bnizh14@freeuni.edu.ge>
2016-07-06 00:37:12 +0400

    araferi

--- database.sql
+++ database.sql
@@ -124,10 +124,7 @@ INSERT INTO categories (categoryName) VALUES ('Hot Meal'),
   ('Drinks'), ('Alcohol'), ('Meat'), ('Fast Food'), ('Other'), ('Desert');
 
 INSERT INTO admins (password, userName, name, typeOfUser, email, mobileNumber, imageUrl) VALUES ("7c4a8d09ca3762af61e59520943dc26494f8941b","super",
-"administrator",3,"maiakovsk@gmail.com","12345678","D:\\prog\\Final-Project\\oop-final\\web\\admin.png"
+"administrator",3,"maiakovsk@gmail.com","12345678","C:\\Users\\Boris\\Desktop\\final project\\oop-final\\admins\\admin.png"
 );
 
-SELECT *
-FROM transactions ;
-select sum(amount) as overall, itemID, sellerID,
-  COUNT(DISTINCT CAST(date AS DATE)) as difDays FROM transactions GROUP BY itemID, itemID ORDER BY overall DESC ;
+select * from admins;
--- src/Managers/SiteConstants.java
+++ src/Managers/SiteConstants.java
@@ -9,12 +9,13 @@ public class SiteConstants {
     public static String USER="user";
     public static String ITEM ="item";
     public static String TYPE="type";
-    public static String IMAGE_DIRECTORY="D:\\\\prog\\\\OOP\\\\files\\\\";
-    public static String ADMIN_IMAGE_DIRECTORY="D:\\\\prog\\\\OOP\\\\files\\\\admin\\\\";
-    public static String DEFAULT_USER_IMAGE="D:\\\\prog\\\\Final-Project\\\\oop-final\\\\web\\\\defaultProfile.png";
-    public static String DEFAULT_ITEM_IMAGE="D:\\\\prog\\\\Final-Project\\\\oop-final\\\\web\\\\defaultitem.png";
-    public static String DEFAULT_ADMIN_IMAGE="D:\\\\prog\\\\Final-Project\\\\oop-final\\\\web\\\\admin.png";
+    public static String IMAGE_DIRECTORY="C:\\\\Users\\\\Boris\\\\Desktop\\\\final project\\\\oop-final\\\\users\\\\";
+    public static String DEFAULT_USER_IMAGE="C:\\\\Users\\\\Boris\\\\Desktop\\\\final project\\\\oop-final\\\\users\\\\defaultProfile.png";
+    public static String DEFAULT_ITEM_IMAGE="C:\\\\Users\\\\Boris\\\\Desktop\\\\final project\\\\oop-final\\\\users\\\\defaultitem.jpg";
     public static int SellerType = 1;
+    public static String ADMIN_IMAGE_DIRECTORY="C:\\\\Users\\\\Boris\\\\Desktop\\\\final project\\\\oop-final\\\\admins\\\\";
+    public static String DEFAULT_ADMIN_IMAGE ="C:\\\\Users\\\\Boris\\\\Desktop\\\\final project\\\\oop-final\\\\admins\\\\admin.png";
+
     public static int BuyerType = 0;
     public static int ADMIN_TYPE = 2;
     public static int SUPER_ADMIN_TYPE = 3;
